9906dbe4599d19222b38e97acea6c7da9d3e09e5
jinoo7099 <vxc7099@gmail.com>
2021-10-30 14:33:23 +0900

    init folder structure

---
+++ src/server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import http from "http";
+import SocketIO from "socket.io";
+// import WebSocket, { WebSocketServer } from "ws";
+
+const app = express();
+
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+// app.get("/", (req, res) => res.render("home"));
+// app.get("/*", (req, res) => res.redirect("/"));
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+
+wsServer.on("connection", (socket) => {
+    socket.on("join_room", (roomName) => {
+        socket.join(roomName);
+        socket.to(roomName).emit("welcome");
+    })
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer", offer);
+    })
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer);
+    })
+    socket.on("ice", (ice, roomName) => {
+        socket.to(roomName).emit("ice", ice);
+    })
+})
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(8000, handleListen);
+
+
+/*
+  랜덤매칭
+  웹기준으로 브라우저 에서 무슨 값을 해쉬로 만듬
+  그래서 앞의 몇자리를
+  room으로 만들고 그걸 랜덤으로 매칭?
+  클라이언트랑 서버랑 연결된 세션id로 room을 만들자?
+
+*/
