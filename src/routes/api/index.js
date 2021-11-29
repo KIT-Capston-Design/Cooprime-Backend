7189fd633d632aad24c2d1c2515c0bb1e8189b89
jinoo7099 <vxc7099@gmail.com>
2021-11-29 23:13:55 +0900

    fix login and delete file

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const app = require("../../server");
 
 router.use("/login", require("./login"));
-router.use("/chatroom", require("./chatroom"));
 
 module.exports = router;
